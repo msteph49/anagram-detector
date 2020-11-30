6ad74d9bba1137e434ad041082883888b982ffea
Michael Stephens <mdstephens26@yahoo.com>
2020-11-29 21:15:56 -0500

    commit tree ff37d39e9dd17384ddbc34acdf2ad846114f6652

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :words
+    def initialize(words)
+        @words=words
+    end
+    
+    def match(argument)
+        argument.select do |words|
+        words.split("").sort == @words.split("").sort
+        end
+    end
+
+
+
+
+end
